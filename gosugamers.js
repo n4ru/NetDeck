24f82c3b9b26f6005faebc44026e22037c0e9c87
n4ru <geokoder@gmail.com>
2014-05-31 01:51:02 -0400

    Added support for GosuGamers. Started base code for deck previews near links.

---
+++ gosugamers.js
@@ -0,0 +1,43 @@
+﻿var decklist = [];
+$('[class^="card-link"]').each(function(i, el) {
+	//$('.card-link card ').each (function(i, el) {
+	var values = $('[class^="name card-quality"]', this).text(); //substring(2).substring(0, $(this).text().length - 10).replace(/[^a-zA-Z0-9\.\s']+/g ,"").split(' ');
+	if ($('.count', this).text() == "2") {
+    decklist.push(values);
+	}
+    decklist.push(values);
+});
+
+
+var data = decklist.join("\r\n");
+var saveData = (function () {
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    return function (data, fileName) {
+        var blob = new Blob([data], {type: "octet/stream"}),
+            url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    };
+}());
+
+
+$(document).ready(function(){
+fileName = $('h2 [class^="class-color"]').text() + '.txt';
+if (window.location.href.indexOf("#ndslink") > -1) {
+    saveData(data, fileName);
+}
+   $(document).on('click', 'a[href="http://' + window.location.hostname + window.location.pathname + '#copylink"]', function(){
+      prompt("Copy and share the link below - Users without the addon will see the normal page", 'http://' + window.location.hostname + window.location.pathname + '#ndslink');
+   }); 
+	   $(document).on('click', 'a[href="http://' + window.location.hostname + window.location.pathname + '#download"]', function(){
+		  saveData(data, fileName);
+   });
+
+   
+});
+
+$('.container').prepend('<div style="position:fixed; top:100px; left:0"><a href="http://' + window.location.hostname + window.location.pathname + '#download"class="download"><img width="100px" height="100px" src="' + chrome.extension.getURL("key.png") + '"></br></a><a href="http://' + window.location.hostname + window.location.pathname + '#copylink" class="copylink"><img width="100px" height="100px" src="' + chrome.extension.getURL("key_2.png") + '"></br></br></a><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8HU8C8R5VNE96"><img width="100px" height="100px" src="' + chrome.extension.getURL("donation.png") + '"></a></br><img width="100px" height="100px" src="' + chrome.extension.getURL("btc.png") + '"></div>');
