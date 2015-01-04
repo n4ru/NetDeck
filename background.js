1d87b1ad1ec593e3c5565cc479b3f36cea6918d8
n4ru <geokoder@gmail.com>
2015-01-04 03:36:39 -0500

    Fixed moronic pageAction behavior.
    No longer shows the pageAction charm on current tab if a Hearthstone
    site was opened out of focus. Will now correctly show the charm on the
    page actually running the content script.

--- background.js
+++ background.js
@@ -5,14 +5,12 @@ chrome.pageAction.onClicked.addListener(function (tab) {
 chrome.runtime.onInstalled.addListener(function (details) {
     if ((details.reason === "install")||(details.reason === "update")) {
         chrome.tabs.create({
-       		'url': chrome.extension.getURL('updates.html')
+       		'url': chrome.extension.getURL('http://n4ru.it/netdek/updates.html')
    		});
 
     }
 });
 
-chrome.extension.onMessage.addListener(function() {
-        chrome.tabs.getSelected(null, function(tabs) {
-			chrome.pageAction.show(tabs.id);
-		});
+chrome.extension.onMessage.addListener(function(req, sender, resp) {
+			chrome.pageAction.show(sender.tab.id);
 });
