b7bacb3b9edc16377edda7cafbbbb602e91c07b4
n4ru <geokoder@gmail.com>
2015-01-14 04:05:33 -0500

    Formatting.

--- jDeck.js
+++ jDeck.js
@@ -35,12 +35,8 @@ siteFunctions = {
   'hearthhead.com/deck=': function() {
   	if ($('[class^="collapsed-card"] > .base').length){
   		deck.addCards('[class^="collapsed-card"] > .base', '.name', '.count');
-		download = function(){
-			deck.download($('.text h1').text() + '.txt');
-		};
-		copy = function(){
-			deck.copy();
-		};
+		download = function(){deck.download($('.text h1').text() + '.txt')};
+		copy = function(){deck.copy()};
 	} else { hideCharm(); }
   },
   'hearthhead.com/deckbuilder': function() {
@@ -71,72 +67,44 @@ siteFunctions = {
 			deck.list.push(values.join(' '));
 		}
 	});
-	download = function(){
-		deck.download($('#wrapper > #center > .headbar > div[style*="float:left"]').text() + '.txt');
-	}
-	copy = function(){
-		deck.copy();
-	};
+	download = function(){deck.download($('#wrapper > #center > .headbar > div[style*="float:left"]').text() + '.txt')};
+	copy = function(){deck.copy()};
   },
   'gosugamers.net/hearthstone/decks/': function() {
 	deck.addCards('[class^="card-link"]', '[class^="name card-quality"]', '.count');
-	download = function(){
-		deck.download($('h2 [class^="class-color"]').text() + '.txt');
-	};
-	copy = function(){
-		deck.copy();
-	};
+	download = function(){deck.download($('h2 [class^="class-color"]').text() + '.txt')};
+	copy = function(){deck.copy()};
   },
   'hearthstoneplayers.com/': function() {
   	if ($('[class^="deck-list"]').length){
 		deck.addCards('.card', '.card-title', '.card-count');
-		download = function(){
-			deck.download($('#post-title').text() + '.txt');
-		};
-		copy = function(){
-			deck.copy();
-		};
+		download = function(){deck.download($('#post-title').text() + '.txt')};
+	copy = function(){deck.copy()};
 	} else { hideCharm(); }
   },
   'hearthpwn.com/decks/': function() {
 	deck.addCards('.even, .odd', 'b', '.col-name');
-	download = function(){
-		deck.download($('.t-deck-title').text() + '.txt');
-	};
-	copy = function(){
-		deck.copy();
-	};
+	download = function(){deck.download($('.t-deck-title').text() + '.txt')};
+	copy = function(){deck.copy()};
   },
   'hearthstats.net/decks/': function() {		
   	if ($('.deckBuilderCardsWrapper').length){
 		deck.addCards('[class^="card cardWrapper"]', '.name', '.qty');
-		download = function(){
-			deck.download($('.page-title').text().replace(/ Deck Views:.*/, "") + '.txt');
-		};
-		copy = function(){
-			deck.copy();
-		};
+		download = function(){deck.download($('.page-title').text().replace(/ Deck Views:.*/, "") + '.txt')};
+		copy = function(){deck.copy()};
   	} else { hideCharm(); }
   },
   'hss.io/decks/': function() {
   	if ($('.deckBuilderCardsWrapper').length){
 		deck.addCards('[class^="card cardWrapper"]', '.name', '.qty');
-		download = function(){
-			deck.download($('.page-title').text().replace(/ Deck Views:.*/, "") + '.txt');
-		};
-		copy = function(){
-			deck.copy();
-		};
+		download = function(){deck.download($('.page-title').text().replace(/ Deck Views:.*/, "") + '.txt')};
+		copy = function(){deck.copy()};
   	} else { hideCharm(); }
   },
   'heartharena.com/arena-run/': function() {
 	deck.addCards('[class^="arenaDeckList arena-section"] > .deckList > .deckCard', '.name', '.quantity');
-	download = function(){
-		deck.download($('.deck-archetype-name').text() + '.txt');
-	};
-	copy = function(){
-		deck.copy();
-	};
+	download = function(){deck.download($('.deck-archetype-name').text() + '.txt')};
+	copy = function(){deck.copy()};
   },
   'tempostorm.com': function() {
 	function poll() {
@@ -149,14 +117,8 @@ siteFunctions = {
 		deck.list = [];
 		deck.addCards('.db-deck-cards > [class^="db-deck-card ng-scope"]', '[class^="db-deck-card-name"]', '[class^="db-deck-card-qty"]');
 	};
-	download = function(){
-		update();
-		deck.download($('h1.ng-binding').text() + '.txt');
-	};
-	copy = function(){
-		update();
-		deck.copy();
-	};
+	download = function(){update();deck.download($('h1.ng-binding').text() + '.txt')};
+	copy = function(){update();deck.copy()};
   },
   'hearthpwn.com/deckbuilder': function() {
   	update = function(){
@@ -169,24 +131,14 @@ siteFunctions = {
 			}
 		});
   	};
-	download = function(){
-		update();
-		deck.download($('.deck-name-container > h2').text() + '.txt');
-	};
-	copy = function(){
-		update();
-		deck.copy();
-	};
+	download = function(){update();deck.download($('.deck-name-container > h2').text() + '.txt')};
+	copy = function(){update();deck.copy()};
   },
   'pro.eslgaming.com/hearthstone/legendary/': function() {
   	if ($('.decklist_compact').length){
 		deck.addCards('.carditem_container', '[class^="name"]', '[class^="num"]');
-		download = function(){
-			deck.download($('h3').text() + '.txt');
-		};
-		copy = function(){
-			deck.copy();
-		};
+		download = function(){deck.download($('h3').text() + '.txt')};
+		copy = function(){deck.copy()};
 	} else { hideCharm(); }
   }
 }
