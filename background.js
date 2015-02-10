9d2fd9ac625f2bcb9f0a95c36aea95526f2fd722
n4ru <geokoder@gmail.com>
2015-02-10 15:47:34 -0500

    Small changes, formatting.

--- background.js
+++ background.js
@@ -1,15 +1,25 @@
-chrome.pageAction.onClicked.addListener(function (tab) {
-	if (!window.jQuery) {chrome.tabs.executeScript(tab.id, {file: 'jq.js'})}
-	chrome.tabs.executeScript(tab.id, {file: 'cards.js'});
-	chrome.tabs.executeScript(tab.id, {file: 'jDeck.js'});
+chrome.pageAction.onClicked.addListener(function(tab) {
+	chrome.tabs.executeScript(tab.id, {
+		file: 'jq.js'
+	}, function() {
+		chrome.tabs.executeScript(tab.id, {
+			file: 'cards.js'
+		}, function() {
+			chrome.tabs.executeScript(tab.id, {
+				file: 'jDeck.js'
+			})
+		})
+	})
 });
 
-chrome.runtime.onInstalled.addListener(function (details) {
-    if ((details.reason === "install")||(details.reason === "update")) {
-        chrome.tabs.create({'url': 'http://netdeck.n4ru.it/'});
-    }
+chrome.runtime.onInstalled.addListener(function(details) {
+	if ((details.reason === "install") || (details.reason === "update")) {
+		chrome.tabs.create({
+			'url': 'http://netdeck.n4ru.it/'
+		});
+	}
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
- 	chrome.pageAction.show(tabId);
+	chrome.pageAction.show(tabId);
 });
