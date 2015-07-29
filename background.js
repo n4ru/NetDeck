2540c021d8236dee78087961f328116274473a76
George Kushnir <admin@n4ru.it>
2015-07-29 17:30:20 -0400

    Context Menus

--- background.js
+++ background.js
@@ -41,17 +41,12 @@ chrome.runtime.onInstalled.addListener(function(details) {
         chrome.tabs.create({
             'url': 'https://netdeck.n4ru.it/getting-started-3/'
         });
-        GET("https://netdeck.n4ru.it/notifs.php", function(data) {
-            newPost = JSON.parse(data.responseText);
-            chrome.storage.sync.set({
-                post: newPost['ID']
-            })
-        })
     } else if (details.reason === "update") {
+        /*
         chrome.notifications.create("update", opt = {
             type: "image",
             title: "NetDeck",
-            message: "NetDeck has been updated to 3.0.4!\nClick to see what's changed!",
+            message: "NetDeck has been updated to 3.0.5!\nClick to see what's changed!",
             iconUrl: "notif.png",
             imageUrl: "update.png"
         }, function() {
@@ -77,7 +72,72 @@ chrome.runtime.onInstalled.addListener(function(details) {
                 }
             })
         })
+        */
     }
+    GET("https://netdeck.n4ru.it/notifs.php", function(data) {
+        var ndcontext = chrome.contextMenus.create({
+            "title": "NetDeck",
+            "id": "ndmenu",
+            "contexts": ["all"]
+        });
+        newPost = JSON.parse(data.responseText);
+        var ndsub2 = chrome.contextMenus.create({
+            "title": "Instant Find-a-Friend",
+            "parentId": ndcontext,
+            "id": "faf",
+            "contexts": ["all"]
+        });
+        var ndsub2 = chrome.contextMenus.create({
+            "title": "Interaction Compendium",
+            "parentId": ndcontext,
+            "id": "ic",
+            "contexts": ["all"]
+        });
+        var ndsub3 = chrome.contextMenus.create({
+            "title": "View Decks",
+            "parentId": ndcontext,
+            "id": "decks",
+            "contexts": ["all"]
+        });
+        var ndsub4 = chrome.contextMenus.create({
+            "type": "separator",
+            "parentId": ndcontext,
+            "id": "sep",
+            "contexts": ["all"]
+        });
+        var ndsub5 = chrome.contextMenus.create({
+            "title": "Feedback",
+            "parentId": ndcontext,
+            "id": "feedback",
+            "contexts": ["all"]
+        });
+        chrome.storage.sync.set({
+            post: newPost['ID']
+        })
+        chrome.contextMenus.onClicked.addListener(function(info, tab) {
+            switch (info.menuItemId) {
+                case "faf":
+                    chrome.tabs.create({
+                        "url": "https://netdeck.n4ru.it/find-a-friend"
+                    })
+                    break;
+                case "ic":
+                    chrome.tabs.create({
+                        "url": "https://netdeck.n4ru.it/interaction-compendium/"
+                    })
+                    break;
+                case "decks":
+                    chrome.tabs.create({
+                        "url": "https://netdeck.n4ru.it/category/decks/"
+                    })
+                    break;
+                case "feedback":
+                    chrome.tabs.create({
+                        "url": "https://netdeck.n4ru.it/feedback/"
+                    })
+            }
+        });
+    })
 });
 popIt = function popIt(id) {
     if (id.indexOf("deck") > -1) {
